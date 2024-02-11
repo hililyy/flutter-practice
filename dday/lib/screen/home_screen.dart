e4e726d9bff856123bbc050f8722c4c9fdbeba31
hililyy <joun406@gmail.com>
2024-02-11 20:35:00 +0900

    [ADD] 디자인 마무리

--- dday/lib/screen/home_screen.dart
+++ dday/lib/screen/home_screen.dart
@@ -13,18 +13,40 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.pink[50],
       body: SafeArea(
+        bottom: false,
         child: Container(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              Text(
-                'U&I',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'parisienne',
-                  fontSize: 80,
-                ),
-              ),
+              _TopPart(),
+              _BottomPart(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'parisienne',
+              fontSize: 80,
+            ),
+          ),
+          Column(
+            children: [
               Text(
                 '우리 처음 만난날',
                 style: TextStyle(
@@ -42,23 +64,38 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               IconButton(
-                onPressed: (){},
+                iconSize: 60,
+                onPressed: () {},
                 icon: Icon(
                   Icons.favorite,
-                ),
-              ),
-              Text(
-                'D+1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sumflower',
-                  fontSize: 50,
-                  fontWeight: FontWeight.w700,
+                  color: Colors.red,
                 ),
               ),
             ],
           ),
-        ),
+          Text(
+            'D+1',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'sumflower',
+              fontSize: 50,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset(
+        'asset/img/middle_image.png',
       ),
     );
   }
